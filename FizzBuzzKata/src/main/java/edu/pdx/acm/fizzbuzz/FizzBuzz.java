addd0e0108f08fc2731a285ee842aeb5f573c8ac
David Whitlock <david.m.whitlock@gmail.com>
2015-11-22 10:22:35 -0800

    Simple unit test that verifies that tests can be run.

---
+++ FizzBuzzKata/src/main/java/edu/pdx/acm/fizzbuzz/FizzBuzz.java
@@ -0,0 +1,7 @@
+package edu.pdx.acm.fizzbuzz;
+
+public class FizzBuzz {
+  public static int echo(int echo) {
+    return echo;
+  }
+}
---
+++ FizzBuzzKata/src/test/java/edu/pdx/acm/fizzbuzz/FizzBuzzTest.java
@@ -0,0 +1,14 @@
+package edu.pdx.acm.fizzbuzz;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class FizzBuzzTest {
+
+  @Test
+  public void echo1is1() {
+    assertEquals(1, FizzBuzz.echo(1));
+  }
+
+}
