8c88e9b914f5d410ed1979e607c444f14ada256f
David Whitlock <david.m.whitlock@gmail.com>
2015-11-22 10:26:30 -0800

    Use hamcrest matchers in the unit tests in order to make them more readable.

--- FizzBuzzKata/src/test/java/edu/pdx/acm/fizzbuzz/FizzBuzzTest.java
+++ FizzBuzzKata/src/test/java/edu/pdx/acm/fizzbuzz/FizzBuzzTest.java
@@ -2,13 +2,14 @@ package edu.pdx.acm.fizzbuzz;
 
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 public class FizzBuzzTest {
 
   @Test
   public void echo1is1() {
-    assertEquals(1, FizzBuzz.echo(1));
+    assertThat(FizzBuzz.echo(1), equalTo(1));
   }
 
 }
